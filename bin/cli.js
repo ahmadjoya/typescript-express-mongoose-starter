9d9882a0b847a7c8859c5087df18575fbe98790a
Ahmad Joya <joya.abn@gmail.com>
2023-05-23 21:29:16 +0430

    feat: cli.js for npx

---
+++ bin/cli.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+/*****************************************************************
+ * TypeScript Express Mongoose Starter
+ * 2023.05.23 ~ 🎮
+ * Made By Ahmad Joya 🦖
+ * https://github.com/ahmadjoya/typescript-express-mongoose-starter
+ *****************************************************************/
+import { execSync } from 'child_process';
+const runCommand = command => {
+  try {
+    execSync(`${command}`, { stdio: 'inherit' });
+  } catch (e) {
+    console.error(`Failed to execute ${command}`);
+  }
+};
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/ahmadjoya/typescript-express-mongoose-starter ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+console.log(`Cloning ${repoName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) process.exit(-1);
+console.log(`Installing dependencies for ${repoName}`);
+const installedDeps = runCommand(installDepsCommand);
+if (!installedDeps) process.exit(-1);
+
+console.log('Congratulations! you are ready. Follow the following command to start');
+console.log(`cd ${repoName} && npm start`);
+
+const path = require('path');
+const starter = require('../lib/starter');
+const destination = getDest(process.argv[2]);
+
+function getDest(destFolder = 'typescript-express-starter') {
+  return path.join(process.cwd(), destFolder);
+}
+
+starter(destination);
