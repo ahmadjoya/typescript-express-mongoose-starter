74b61c6cdd009b7d195b64c6d161a6d0c801fcf1
Ahmad Joya <joya.abn@gmail.com>
2023-05-23 22:16:53 +0430

    fix: npx run command

--- bin/cli.js
+++ bin/cli.js
@@ -26,13 +26,3 @@ if (!installedDeps) process.exit(-1);
 
 console.log('Congratulations! you are ready. Follow the following command to start');
 console.log(`cd ${repoName} && npm start`);
-
-const path = require('path');
-const starter = require('../lib/starter');
-const destination = getDest(process.argv[2]);
-
-function getDest(destFolder = 'typescript-express-starter') {
-  return path.join(process.cwd(), destFolder);
-}
-
-starter(destination);
